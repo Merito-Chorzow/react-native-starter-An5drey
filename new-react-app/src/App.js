14f146d10a47ea1488e8b0f2d7d9b65efc4a27ef
Andrii Popchuk <andriy.popchuk@gmail.com>
2025-10-31 22:46:55 +0100

    done

--- new-react-app/src/App.js
+++ new-react-app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import StartPage from "./components/StartPage";
+import AddPage from "./components/AddPage";
+import NotesList from "./components/NotesList";
+import NoteDetails from "./components/NoteDetails";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="nav">
+        <Link to="/">Start</Link>
+        <Link to="/add">Dodaj</Link>
+        <Link to="/list">Wspomnienia</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/add" element={<AddPage />} />
+        <Route path="/list" element={<NotesList />} />
+        <Route path="/details/:id" element={<NoteDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
